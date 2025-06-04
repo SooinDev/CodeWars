23cd131a51665e6ed7c95926f1f6a334a4abcea7
sooin <alwayswithsound@gmail.com>
2025-06-05 01:27:10 +0900

    Add solution - CodeHub

---
+++ 8-kyu/convert-a-number-to-a-string/convert-a-number-to-a-string.java
@@ -0,0 +1,6 @@
+class Kata {
+    public static String numberToString(int num) {
+        String answer = Integer.toString(num);
+        return answer; // Return a string of the number here!
+    }
+}
