4d1c8e6852ea31bf70e1d008a6edc343e0a38da4
sooin <alwayswithsound@gmail.com>
2025-06-07 10:15:43 +0900

    Add solution - CodeHub

---
+++ 8-kyu/counting-sheep-dot-dot-dot/counting-sheep-dot-dot-dot.java
@@ -0,0 +1,11 @@
+public class Counter {
+    public static int countSheeps(Boolean[] arrayOfSheeps) {
+        int count = 0;
+        for (int i = 0; i < arrayOfSheeps.length; i++) {
+            if (arrayOfSheeps[i] != null && arrayOfSheeps[i] == true)
+                count++;
+        }
+​
+        return count;
+    }
+}
